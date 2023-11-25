9fe674587f5e1bff2accf94f810893d401090559
HeeJu Cho <121915790+he2joojo@users.noreply.github.com>
2023-11-26 00:54:24 +0900

    leet: 31. Next Permutation

--- leetCode/031_NextPermutation.py
+++ leetCode/031_NextPermutation.py
@@ -3,6 +3,11 @@ from typing import List
 
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
+        # 1. 뒤에서부터 <가 되는 idx 찾기
+        # 2. idx = 0이면 순열의 끝이므로 reverse() 반환
+        # 3. nums[idx-1]이 바뀌어야 하는 자리
+        # 4. nums[idx:] 맨 뒤부터 swap < nums[i]찾아서 바꾸고
+        # 5. nums[idx:]는 정렬
         idx = 0
         for i in range(len(nums) -1, 0, -1):
             if nums[i] > nums[i - 1]:
