48a7abafb0ce2623539b080b69ed1b3bc15752f8
HeeJu Cho <121915790+he2joojo@users.noreply.github.com>
2024-01-02 04:08:45 +0900

    programmers: 불량사용자

---
+++ programmers/blockUser.py
@@ -0,0 +1,32 @@
+from itertools import permutations
+
+
+def solution(user_id, banned_id):
+    # 순열 리스트 구하기
+    cases = list(permutations(user_id, len(banned_id)))
+    answer = []
+    for case in cases:
+        if check(case, banned_id):
+            case = set(case)    # set()해주면 자동으로 정렬해줌!
+            if case not in answer:
+                answer.append(case)
+
+    return len(answer)
+
+
+def check(users, blocks):
+    for i in range(len(blocks)):
+        block = blocks[i]
+        user = users[i]
+        if len(block) != len(user):
+            return False
+        for j in range(len(block)):
+            if block[j] == '*':
+                continue
+            if block[j] != user[j]:
+                return False
+
+    return True
+
+
+print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "*rodo", "******", "******"]))
